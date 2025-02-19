27c5535b36d29992c8fd7561d10208add4e4fdff
bom5106 <78384383+bom5106@users.noreply.github.com>
2025-02-18 23:22:45 -0500

    Add files via upload

---
+++ Client Programming/C#/endPoint/endPoint/Controllers/NewController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace endPoint.Controllers
+{
+    public class NewController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
