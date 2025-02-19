39e4035c5f5559ca2b329eba00bc2aa8afc58641
bom5106 <78384383+bom5106@users.noreply.github.com>
2025-02-18 23:38:57 -0500

    Add files via upload

---
+++ Client Programming/C#/MyFirstConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace myFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Give me your name ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+            //or
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}");
+        }
+    }
+}
